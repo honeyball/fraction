ab2d2cd2eab7b9133f80ea2a479824d86020adee
honeyball <vladislavivanov2745@gmail.com>
2015-11-09 14:12:07 +0800

    Update and rename LICENSE.md to Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,48 @@
+public class Fraction {
+    int n, d;
+
+    Fraction (int num, int den)
+    {
+        n = num;
+        d = den;
+    }
+    void Simplify()
+    {
+        n = n / nok(n,d);
+        d = d / nok(n,d);
+    }
+
+    public String toString()
+    {
+        return n + "/" + d;
+    }
+    public Fraction add(Fraction x)
+    {
+        return new Fraction(n * nok(d,x.d) + x.n * nok(d,x.d), nok(d,x.d));
+    }
+    
+    public Fraction sub(Fraction x)
+    {
+    	return new Fraction(n * nok(d,x.d) - x.n * nok(d,x.d), nok(d,x.d));
+    }
+    
+    public Fraction mul(Fraction x)
+    {
+        return new Fraction(n*x.n, d*x.d);
+    }
+    
+    public Fraction div(Fraction x)
+    {
+        return new Fraction(n*x.d, d*x.n);
+    }
+
+    private static int nod(int a, int b) {
+        if (b == 0) return a;
+        int x = a % b;
+        return nod(b, x);
+    }
+    
+    private static int nok(int a,int b){
+    	return (a * b) / (nod(a,b));
+    }
+}
